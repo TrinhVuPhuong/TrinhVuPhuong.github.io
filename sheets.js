280367d863ce71a98adcebf0314d5b18cb35a4d1
TrinhVuPhuong <sipnhocprooo@gmail.com>
2024-10-14 12:33:05 +0700

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -407,7 +407,7 @@ function maker() {
             x = obj['thanhtien'];
             //x = x.toLocaleString('en-US');
             td = document.createElement('td');
-            td.setAttribute("class", "content");
+            td.setAttribute("class", "title3");
             td.textContent=  x + " đ";
             tr.append(td);
 
